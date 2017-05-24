4173503da9aedb25e7045e4e64c3e1ac2e426efc
Andrew Chilton <andychilton@gmail.com>
2017-05-24 23:59:37 +1200

    Update build instructions

--- docs/create-a-release.sh
+++ docs/create-a-release.sh
@@ -5,6 +5,7 @@ First thing to do is bump the version number in the `package.json`. Adhere to [S
 Then:
 
 ```
+
 echo "Getting release information ..."
 VERSION=`jq --raw-output .version package.json`
 DATE=`date +'%Y-%m-%d'`
@@ -15,18 +16,32 @@ git add package.json
 git commit -m "Release v$VERSION - $DATE"
 echo
 
+```
+
+And check it looks okay:
+
+```
+
 git show | cat
+
 ```
 
 The only change in this commit is the "version" line of `package.json`.
 
-If that all looks okay, do a `git push origin master`.
+If that all looks okay too, do a:
+
+```
+
+git push origin master
+
+```
 
 ## Build ##
 
 Now build and tag the release:
 
 ```
+
 echo "Building the project for release ..."
 node build.js
 git add dist/
@@ -35,6 +50,7 @@ git push origin master
 git tag v$VERSION
 git push origin v$VERSION
 echo
+
 ```
 
 ## Release ##
@@ -42,6 +58,7 @@ echo
 Here's a small script to follow so that you don't push to npm anything that shouldn't be there:
 
 ```
+
 echo "Getting dir information ..."
 CURRENT_DIR=`pwd`
 DIR_NAME=`basename $CURRENT_DIR`
@@ -62,6 +79,7 @@ cd ../
 rm -rf $DIR_NAME
 cd $CURRENT_DIR
 echo
+
 ```
 
 (Ends)
